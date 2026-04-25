b11adf80be535eed5b48b192fbdd8a26469d14d1
masakadop <69153465+masakadop@users.noreply.github.com>
2026-04-25 09:53:12 +0900

    Update footer section with potential incorrect facility information notice and contact guidance.

---
+++ components/penguin-facilities-page.tsx
@@ -0,0 +1,5 @@
+<footer>
+  <p>The World Penguin Day is celebrated every year on April 25th to raise awareness about the conservation of penguins and their habitats. </p>
+  <p>Please be aware that the information regarding facilities may potentially contain errors. We recommend checking official websites for the most accurate and updated details.</p>
+  <p>If you find any incorrect information, please feel free to contact us via pull request.</p>
+</footer>
